01646d60d96d83650176c05898810f223cfc3385
Brian Luc <brian.luc.95@outlook.com>
2025-10-20 21:21:19 +1100

    Added CORS

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	"our-home-server/db"
@@ -11,6 +12,10 @@ func main() {
 	db.InitPostgresDb()
 
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Content-Type"}}))
 	routers.InitItemsRouter(r)
 	routers.InitRoomsRouter(r)
 	routers.InitCommentsRouter(r)
